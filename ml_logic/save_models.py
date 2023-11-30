9e17b42d3ed6fce1de59523564e173df10fc818a
Ekaterina Bulaeva <katebulaeva@gmail.com>
2023-11-30 17:27:48 +0100

    NLP modified, New file for saving models, NLP columns added to workflow local

---
+++ ml_logic/save_models.py
@@ -0,0 +1,18 @@
+from RateMate.ml_logic.NLP import pretraining_NLP_models, get_dataset_for_NLP, get_Xy_for_NLP
+import pandas as pd
+
+
+# Take user input for the file path or take the default path
+default_file_path = "./raw_data_slim/merged_slim_file.csv"
+
+path = "Ratemate/raw_data/merged_slim_file.csv" #just for Katya
+
+file_path = input(f"Enter full dataset for training with path, default value = : '{default_file_path}'") or default_file_path
+
+# Load the dataset
+print("Loading dataset...")
+your_dataset = pd.read_csv(file_path)
+print("Data loaded ✅")
+data, df_full = get_dataset_for_NLP(your_dataset)
+X, y = get_Xy_for_NLP(data)
+pretraining_NLP_models(X,y)
--- workflow_local.py
+++ workflow_local.py
@@ -3,6 +3,9 @@ import pandas as pd
 from ml_logic.text_preprocessor import TextPreprocessor
 from ml_logic.bert_nlp_classification import process_reviews
 from ml_logic.calculate_final_score import *
+from ml_logic.NLP import *
+
+
 
 # Take user input for the file path or take the default path
 default_file_path = "./raw_data_slim/merged_slim_file.csv"
@@ -33,9 +36,19 @@ print("Classified reviews ✅")
 print(result_df_small.head(3))
 
 # Calculate the final score
+
 print("Calculating final score...")
-sub_ratings_df = fill_sub_ratings(result_df_small, only_price=False)
+
+
+sub_ratings_df = fill_sub_ratings(result_df_small, only_price=True)
+try:
+    sub_ratings_df = new_column_NLP(sub_ratings_df)
+except:
+    print("********DO 'save_models.py' first******")
+
 print(sub_ratings_df.head(3))
+
+
 
 final_score_df = df_with_score(sub_ratings_df, price_weight=0.25, service_weight=0.25, atmosphere_weight=0.25, food_weight=0.25)
 print(f"Dataset with final scores {final_score_df.head(3)}")
