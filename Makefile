5f6591567298870772944d4ea6485665cf9adfcf
Phani Pravallika Kollipara <phani.iae@gmail.com>
2023-12-04 18:44:28 +0100

    Updated steamlit

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-.DEFAULT_GOAL := run_streamlit
+.DEFAULT_GOAL := run_streamlit_copy_old
 run_api:
 	uvicorn api.fast:app --reload
+
+run_streamlit_copy_old:
+	streamlit run streamlit/app_copy_old.py
 
 run_streamlit:
 	streamlit run streamlit/app.py
---
+++ streamlit/app_copy_old.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import requests
+
+from ml_logic.scrape_apify import scrape_apify
+from ml_logic.NLP import new_column_NLP
+from ml_logic.calculate_final_score import *
+from interface.main import *
+from params import *
+
+# ####### TO Dos #########
+# """
+# 1. Show the original score
+# 2. Show the subratings from the dataset
+# 3. Integrate the Word cloud picture from my notebook somehow into the streamlit app
+# 4. Add more features
+# """
+st.snow()
+
+st.markdown("""# RateMate
+## Get your personal rating for the restaurant of your choice
+Hello friend, please insert the google maps url of the restaurant you are interested in:""")
+
+# get the user input
+restaurant_url = st.text_input("url of your restaurant")
+
+# the selected value is returned by st.slider
+price_review_weightage = st.slider('Select your price rating weight', 0.0, 1.0, 0.25)
+food_review_weightage = st.slider('Select your food rating weight', 0.0, 1.0, 0.25)
+service_review_weightage = st.slider('Select your service rating weight', 0.0, 1.0, 0.25)
+ambience_review_weightage = st.slider('Select your ambience rating weight', 0.0, 1.0, 0.25)
+
+# checkbox for local guides
+local_guides_review_weightage = st.checkbox('Review only from local guides')
+
+
+if st.button("Get Score"):
+    #url = 'https://awtestratemate2-z2kqlvo2ta-ew.a.run.app/scrape'
+
+    params = {
+            'url': restaurant_url
+            }
+    url = restaurant_url
+    price = price_review_weightage
+    food = food_review_weightage
+    service = service_review_weightage
+    ambience = ambience_review_weightage
+
+    # Make API request
+    #response = requests.get(url, params=params)
+    df = scrape_apify(url)
+    df = df[COLUMN_NAMES_RAW]
+    pre_processed_df = preprocess_reviews_text(df) # Still need to do the column selection
+
+    # Classification of reviews
+    classified_df = classify_reviews_df(pre_processed_df, "reviews_without_SW")
+
+    # Get subratings
+    # Load the production model and calculate subratings (without the price subrating)
+    # Fill n the price subrating after figuring it out
+    # In the end don't use fill_sub_ratings function
+
+    # model = app.state.model
+    # assert model is not None
+    #subratings_df = new_column_NLP(classified_df)
+
+    #subratings_df = create_sub_ratings(classified_df) # This is a place holder
+    #subratings_df_price = df_with_price_rating(subratings_df)
+
+    #subratings_df = create random sub_ratings
+    subratings_df_random = fill_sub_ratings(classified_df)
+
+    # Average scores
+    average_scores_df = calculate_average_scores(subratings_df_random, price, service, ambience, food)
+
+    # Overall score
+    overall_score = calculate_overall_score(average_scores_df)
+
+    # Original score
+    original_score = round(df.stars.mean(), 2)
+
+    st.divider()
+    st.header(f'Restuarant Rating: ⭐️ {original_score} ⭐️')
+
+    st.header(f'Your Personal Score: ⭐️ {overall_score} ⭐️')
